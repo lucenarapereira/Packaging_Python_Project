5646ac71de67f575aa0a2d228cc8e2cf54e1d1d2
Lucy <93477361+lucenarapereira@users.noreply.github.com>
2024-10-30 17:56:36 -0300

    Add files via upload

--- image-processing-package/setup.py
+++ image-processing-package/setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image-processing-clahe",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="lucenara_pereira",
+    author_email="lucenarapereira@gmail.com",
+    description="Algoritmo de processamento de imagens CLAHE (Contrast Limited Adaptive Histogram Equalization).",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/lucenarapereira/Packaging_Python_Project.git"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
