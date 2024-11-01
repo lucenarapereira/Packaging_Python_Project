8cdb4798d29b437e6036a04496ea1b1265998e28
Lucy <93477361+lucenarapereira@users.noreply.github.com>
2024-10-31 22:20:36 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,16 @@
+import sys
+print(sys.path)
+
+import os
+from image_processing_clahe.algoritmo import process_image
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'image_processing_clahe')))
+
+# Altere para o caminho da sua imagem ou forneça como argumento
+if len(sys.argv) > 1:
+    image_path = sys.argv[1]  
+else:
+    image_path = 'C:\\Users\\lucenara.pereira\\Downloads\\Bootcamp_Engenharia_Dados\\2. Pacote_Python\\image-processing-package\\cactos.jpg'  # Caminho padrão
+
+# Chama a função para processar a imagem
+process_image(image_path)
